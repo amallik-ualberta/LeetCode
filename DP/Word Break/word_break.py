c4513b670e258cd5969932800b5c1c16bab10f04
amallik-ualberta <amallik@ualberta.ca>
2020-01-04 18:32:24 -0700

    word break

---
+++ DP/Word Break/word_break.py
@@ -0,0 +1,68 @@
+
+def word_break(s, wordDict):
+
+	if (s== None or wordDict == None):
+
+		return False;
+
+	length = len(s)
+
+	table = []
+
+	for i in range (length):
+
+		table.append(False)
+
+	
+	if(s[0] in wordDict):
+
+		table[0] = True
+
+	for i in range (1, length):
+
+		this_string = s[0:i+1]
+
+		if(this_string in wordDict):
+
+			
+			table[i] = True;
+
+			continue;
+
+		for j in range (0, i):
+
+			this_string = s[j+1 : i+1]
+
+			if (table[j] == True and this_string in wordDict):
+
+				
+				table[i] = True
+
+
+
+	return table[length-1]
+
+
+	
+
+
+
+
+def main():
+
+	s = str(input())
+
+	n = int(input())
+
+	wordDict = []
+
+	for i in range (n):
+
+		element = str(input())
+
+		wordDict.append(element)
+
+	print(word_break(s, wordDict))
+
+if __name__ == "__main__":
+    main()
